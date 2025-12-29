0ab5b2cb37d2b6dccd1060bb96a3dca6dfcd9f39
looooj <2553895012@qq.com>
2025-12-29 08:24:50 +0800

    add js_module_loader_v1 for new version

--- libqjs/examples/hello.cpp
+++ libqjs/examples/hello.cpp
@@ -2,6 +2,13 @@
 #include <quickjs-libc.h>
 #include <string>
 
+#ifndef FALSE
+enum {
+    FALSE = 0,
+    TRUE = 1,
+};
+#endif
+
 void qjs_dump_object(JSContext* ctx, JSValueConst val, std::string& result) {
     const char* str;
     str = JS_ToCString(ctx, val);
@@ -43,11 +50,12 @@ void qjs_dump_error(JSContext* ctx, std::string& result) {
 
 void qjs_run_file(JSContext* ctx, const std::string& filename) {
 
-    size_t file_size=0;
+    size_t file_size = 0;
     uint8_t* text = js_load_file(ctx, &file_size, filename.c_str());
 
     int flags = JS_EVAL_TYPE_MODULE;
-    JSValue ret = JS_Eval(ctx, (const char*)text, file_size, filename.c_str(), flags);
+    JSValue ret =
+        JS_Eval(ctx, (const char*)text, file_size, filename.c_str(), flags);
 
     if (JS_IsException(ret)) {
 
@@ -57,13 +65,41 @@ void qjs_run_file(JSContext* ctx, const std::string& filename) {
     }
 
     js_free(ctx, text);
-    JS_FreeValue(ctx,ret);
+    JS_FreeValue(ctx, ret);
 }
 
+void js_create_object(JSContext* ctx, const std::string& name) {
+}
 
-void js_create_object(JSContext* ctx,const std::string& name)  {
+JSModuleDef* js_module_loader_v1(JSContext* ctx, const char* module_name,
+                                 void* opaque) {
+    JSModuleDef* m;
+    int res;
 
+    size_t buf_len;
+    uint8_t* buf;
 
+    buf = js_load_file(ctx, &buf_len, module_name);
+    if (!buf) {
+        JS_ThrowReferenceError(ctx, "could not load module filename '%s'",
+                               module_name);
+        return NULL;
+    }
+
+    JSValue func_val;
+    /* compile the module */
+    func_val = JS_Eval(ctx, (char*)buf, buf_len, module_name,
+                       JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
+    js_free(ctx, buf);
+    if (JS_IsException(func_val))
+        return NULL;
+    /* XXX: could propagate the exception */
+    js_module_set_import_meta(ctx, func_val, TRUE, FALSE);
+    /* the module is already referenced, so we must free it */
+    m = (JSModuleDef*)JS_VALUE_GET_PTR(func_val);
+    JS_FreeValue(ctx, func_val);
+
+    return m;
 }
 
 void test_01() {
@@ -72,7 +108,7 @@ void test_01() {
     js_std_init_handlers(rt);
     auto ctx = JS_NewContext(rt);
 
-    JS_SetModuleLoaderFunc(rt, NULL, js_module_loader, NULL);
+    JS_SetModuleLoaderFunc(rt, NULL, js_module_loader_v1, NULL);
 
     js_init_module_std(ctx, "std");
     js_init_module_os(ctx, "os");
@@ -87,14 +123,13 @@ void test_01() {
         qjs_dump_error(ctx, result);
         printf("exception\n %s\n", result.c_str());
     }
-    JS_FreeValue(ctx,val);
+    JS_FreeValue(ctx, val);
 
     qjs_run_file(ctx, "hello.js");
     qjs_run_file(ctx, "hello2.js");
 
-
     auto v = JS_NewObject(ctx);
-    JS_FreeValue(ctx,v);
+    JS_FreeValue(ctx, v);
     js_std_free_handlers(rt);
 
     JS_FreeContext(ctx);
